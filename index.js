4f1fc4f8095031df1d38d66dcd052f3e5a162e30
Emmah Gicheru <gicheruemmah@gmail.com>
2025-04-05 00:45:48 +0300

    Complete loop lab code grade

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+console.log(writeCards(["Leon", "Leilani", "Llewellyn"], "birthday"));
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+countDown(10);
